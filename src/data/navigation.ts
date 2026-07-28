65f9154bd28e8b2b6bd0064c2b532ee7d514090a
shelner <shelnerio@gmail.com>
2026-07-28 20:46:50 +0700

    content(navigation): simplify main navigation to primary core sections to reduce scroll fatigue

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -3,9 +3,5 @@ export const navigation = [
   { name: "Expertise", href: "/#expertise" },
   { name: "Projects", href: "/#projects" },
   { name: "Experience", href: "/#experience" },
-  { name: "Certifications", href: "/#certifications" },
-  { name: "Technology", href: "/technology" },
-  { name: "Articles", href: "/#articles" },
-  { name: "About", href: "/#about" },
   { name: "Contact", href: "/#contact" },
 ];
